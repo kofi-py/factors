23ac194197ef235a51973222bcd046e769f78286
Kofi Adu-Boateng <pastorkofi101@gmail.com>
2022-12-20 21:01:06 -0500

    Add unit test file factors_test.py

---
+++ factors_test.py
@@ -0,0 +1,9 @@
+import unittest
+from factors import find_factors
+
+
+class TestFactors(unittest.TestCase):
+    def test_basic(self):
+        testcase = 24
+        expected = [1, 2, 3, 4, 6, 8, 12, 24]
+        self.assertEqual(find_factors(testcase), expected)
